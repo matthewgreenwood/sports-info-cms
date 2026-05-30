cb1405c18cc4063b993c54c1239c8c8ac353e321
britishgymnasticseventsuser <bgscoredev@british-gymnastics.org>
2026-05-30 16:01:38 +0100

    Enable remote data transfer

--- config/server.js
+++ config/server.js
@@ -4,6 +4,11 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  transfer: {
+    remote: {
+      enabled: true,
+    },
+  },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
