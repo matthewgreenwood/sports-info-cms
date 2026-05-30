29e84b22d68d1ade15b215719628f9633d185f38
britishgymnasticseventsuser <bgscoredev@british-gymnastics.org>
2026-05-30 15:25:10 +0100

    Fix upload extension for Strapi v5 controllers

--- src/extensions/upload/strapi-server.js
+++ src/extensions/upload/strapi-server.js
@@ -24,20 +24,34 @@ const shouldUseHotelsPath = (body = {}) => {
 };
 
 module.exports = (plugin) => {
-  const originalUpload = plugin.controllers.upload.upload;
+  const wrapUploadHandler = (controllerKey) => {
+    const controller = plugin?.controllers?.[controllerKey];
 
-  plugin.controllers.upload.upload = async (ctx) => {
-    const body = ctx.request?.body;
-
-    if (body && shouldUseHotelsPath(body)) {
-      ctx.request.body = {
-        ...body,
-        path: HOTELS_UPLOAD_PATH,
-      };
+    if (!controller || typeof controller.upload !== 'function') {
+      return;
     }
 
-    return originalUpload(ctx);
+    const originalUpload = controller.upload;
+
+    controller.upload = async function uploadWithHotelsPath(ctx) {
+      const body = ctx.request?.body;
+
+      if (body && shouldUseHotelsPath(body)) {
+        ctx.request.body = {
+          ...body,
+          path: HOTELS_UPLOAD_PATH,
+        };
+      }
+
+      return originalUpload.call(this, ctx);
+    };
   };
+
+  // Strapi v4: controllers.upload.upload
+  wrapUploadHandler('upload');
+  // Strapi v5: controllers['content-api'].upload and controllers['admin-upload'].upload
+  wrapUploadHandler('content-api');
+  wrapUploadHandler('admin-upload');
 
   return plugin;
 };
