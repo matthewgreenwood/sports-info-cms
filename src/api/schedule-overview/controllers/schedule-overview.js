92c90f687bcbff2c7afbb63aef5c913faeb81fbb
matthewgreenwood <72888952+matthewgreenwood@users.noreply.github.com>
2026-08-18 14:55:23 +0100

    Populate schedule item types in overview

--- src/api/schedule-overview/controllers/schedule-overview.js
+++ src/api/schedule-overview/controllers/schedule-overview.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::schedule-overview.schedule-overview');
+module.exports = createCoreController('api::schedule-overview.schedule-overview', () => ({
+	async find(ctx) {
+		ctx.query = {
+			...ctx.query,
+			populate: {
+				schedule_overview_row: {
+					populate: {
+						schedule_item_types: true,
+					},
+				},
+			},
+		};
+
+		return super.find(ctx);
+	},
+}));
